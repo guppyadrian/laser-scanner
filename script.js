53201e0ecb0628c95d0bd55ff0d02b46f1877f1a
guppy adrian <82202242+guppyadrian@users.noreply.github.com>
2022-06-24 19:09:17 +0000

    working raycasting

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('test')
 
 //draw time
 const ctx = canv.getContext('2d');
-const Cam = {x: 0, y: 0, z: 1};
+const Cam = {x: 0, y: 0, z: 4};
 
 const logPos = {x: 150, y: 150};
 
@@ -51,42 +51,55 @@ function tick() {
 
   
   for (const scanner of ScannerList) {
-    //point towards (0, 0)
+    //point towards log
     scanner.dir = Math.atan2(scanner.origin.x - logPos.x, -scanner.origin.y + logPos.y) * 180 / Math.PI;
-  
-    //get points to draw
-    let scanPoints = scanner.getPoints();
-  
+
+    
+    
     //raycasting
-    for (let i = -50; i < 50; i++) {
+    for (let i = -29; i < 29; i += 0.1) {
       let d = 0;
-      while (d < 100) {
-        d++;
+
+      //store cos/sin for optimization
+      const COS = Math.cos((scanner.dir + i + 90) * Math.PI / 180);
+      const SIN = Math.sin((scanner.dir + i + 90) * Math.PI / 180);
+
+      //d = ray distance. sends out ray until hits log or length > 45
+      while (d < 45) {
+        if (Math.sqrt((scanner.origin.x + d * COS - logPos.x) ** 2 + (scanner.origin.y + d * SIN - logPos.y) ** 2) < 15) {
+          d -= 0.1;
+          break;
+        }
+          
+        d += 0.1;
       }
-      
+
+
+      //counter fisheye
+      const dx = d * (1 / Math.cos(i * Math.PI / 180));
+      const dy = 15 * (1 / Math.cos(i * Math.PI / 180));
+
+      //draw ray
       ctx.beginPath();
-      ctx.moveTo((scanner.origin.x - Cam.x) * Cam.z, (scanner.origin.y - Cam.y) * Cam.z);
-      ctx.lineTo((scanner.origin.x + d * Math.cos((scanner.dir + i) * Math.PI / 180) - Cam.x) * Cam.z, (scanner.origin.y + d * Math.sin((scanner.dir + i) * Math.PI / 180)) - Cam.y) * Cam.z;
+      ctx.moveTo((scanner.origin.x + dy * COS - Cam.x) * Cam.z, (scanner.origin.y + dy * SIN - Cam.y) * Cam.z);
+      ctx.lineTo((scanner.origin.x + dx * COS - Cam.x) * Cam.z, (scanner.origin.y + dx * SIN - Cam.y) * Cam.z);
       ctx.stroke();
     }
     
   
-    //fill
-    ctx.globalAlpha = 0.8;
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = 'black';
   
     //center dot
     ctx.fillRect((scanner.origin.x - 1 - Cam.x) * Cam.z, (scanner.origin.y - 1 - Cam.y) * Cam.z, 2 * Cam.z, 2 * Cam.z);
   }
-  
+
+
+  //draw log
   ctx.beginPath();
   ctx.arc((logPos.x - Cam.x) * Cam.z, (logPos.y - Cam.y) * Cam.z, 15 * Cam.z, 0, 2 * Math.PI);
   ctx.stroke();
 }
 
+// zoom & pan handlers
 addEventListener('wheel', zoom => {
   Cam.z -= zoom.deltaY * 0.001;
 })
@@ -137,5 +150,34 @@ settings:
 - log min/max size
 - log offset
 - 
+
+*/
+
+/*
+ size: 200px
+log at 100px
+zoom in 50%
+log at 150px
+move camera 50px
+
+size 200px
+log at 50px
+zoom to 200%
+log at 100px
+move camera 50px
+
+size 200px
+log at 150px
+camX at 50px
+zoom to 200%
+log now viewed 200px
+camX - 100
+
+size 200px
+log at 50px
+zoom at 200%
+log at 100px
+zoom to 300%
+log now at 150px
 
 */
